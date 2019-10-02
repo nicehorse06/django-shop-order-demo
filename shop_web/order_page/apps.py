07e85229c5c1f75706349747646a63979396df85
Jimmy Ma <a468963@gmail.com>
2019-10-02 22:20:26 +0800

    init shop web

---
+++ shop_web/order_page/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OrderPageConfig(AppConfig):
+    name = 'order_page'
---
+++ shop_web/order_page/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True, verbose_name=u'商品id')
+    stock_pcs = models.PositiveIntegerField(verbose_name=u'商品庫存數量')
+    price = models.PositiveIntegerField(verbose_name=u'商品單價')
+    shop_id = models.CharField(max_length=5, verbose_name=u'商品所屬館別')
+    vip = models.BooleanField(
+        default=True, verbose_name=u'是否為VIP', help_text=u'True => VIP限定/ False =>無限制購買對象')
+
+    class Meta:
+        verbose_name = u'商品'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return u'商品ID(%d)館別(%s)' % (self.product_id, self.shop_id)
+
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name=u'訂單id')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, verbose_name=u'商品')
+    qty = models.PositiveIntegerField(verbose_name=u'購買數量')
+    price = models.PositiveIntegerField(verbose_name=u'商品單價')
+    shop_id = models.CharField(max_length=5, verbose_name=u'商品所屬館別')
+
+    class Meta:
+        verbose_name = u'訂單'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return u'訂單ID(%s)' % self.id
+
+    def save(self, *args, **kwargs):
+        self.price = self.product.price
+        self.shop_id = self.product.shop_id
+        super(Order, self).save(*args, **kwargs)
