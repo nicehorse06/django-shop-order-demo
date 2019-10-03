eb8068b481ca0be57b71c0eeb51c86d63564378a
Jimmy Ma <a468963@gmail.com>
2019-10-03 21:31:20 +0800

    add order list view

--- shop_web/order_page/views.py
+++ shop_web/order_page/views.py
@@ -6,15 +6,18 @@ from .models import Product, Order
 
 class OrderView(View):
     template_name = 'order.html'
-    product_list = Product.objects.all()
 
     def get(self, request):
         form = OrderPostForm()
+        product_list = Product.objects.all()
+        order_list = Order.objects.all()
 
-        return render(request, self.template_name, {"form": form, 'product_list': self.product_list})
+        return render(request, self.template_name, {"form": form, 'product_list': product_list, 'order_list': order_list})
 
     def post(self, request):
         form = OrderPostForm(request.POST)
+        product_list = Product.objects.all()
+        order_list = Order.objects.all()
         if form.is_valid():
             # 如果form中的資料皆合法則is_valid為True，cleaned_data，如果驗證失敗form.cleaned_data只會有驗證通過的數據
             cd = form.cleaned_data
@@ -24,4 +27,4 @@ class OrderView(View):
                 customer_id=cd['customer_id'],
             )
             this_order.save()
-        return render(request, self.template_name, {"form": form, 'product_list': self.product_list})
+        return render(request, self.template_name, {"form": form, 'product_list': product_list, 'order_list': order_list})
