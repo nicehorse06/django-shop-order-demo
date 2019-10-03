e90ac9fa799ba4458c8f119112ab73960c19abbb
Jimmy Ma <a468963@gmail.com>
2019-10-04 00:10:16 +0800

    重構view

--- shop_web/order_page/views.py
+++ shop_web/order_page/views.py
@@ -8,32 +8,29 @@ class OrderView(View):
     template_name = 'order.html'
 
     def dispatch(self, *args, **kwargs):
+        self.form = OrderPostForm()
+        self.product_list = Product.objects.all()
+        self.order_list = Order.objects.all()
         method = self.request.POST.get('_method', '').lower()
         if method == 'delete':
             return self.delete(*args, **kwargs)
         return super(OrderView, self).dispatch(*args, **kwargs)
 
     def get(self, request):
-        form = OrderPostForm()
-        product_list = Product.objects.all()
-        order_list = Order.objects.all()
-
-        return render(request, self.template_name, {"form": form, 'product_list': product_list, 'order_list': order_list})
+        return render(request, self.template_name, {"form": self.form, 'product_list': self.product_list, 'order_list': self.order_list})
 
     def post(self, request):
-        form = OrderPostForm(request.POST)
-        product_list = Product.objects.all()
-        order_list = Order.objects.all()
-        if form.is_valid():
+        this_form = OrderPostForm(request.POST)
+        if this_form.is_valid():
             # 如果form中的資料皆合法則is_valid為True，cleaned_data，如果驗證失敗form.cleaned_data只會有驗證通過的數據
-            cd = form.cleaned_data
+            cd = this_form.cleaned_data
             this_order = Order(
                 product_id=cd['product_id'],
                 qty=cd['quantity'],
                 customer_id=cd['customer_id'],
             )
             this_order.save()
-        return render(request, self.template_name, {"form": form, 'product_list': product_list, 'order_list': order_list})
+        return self.get(request)
 
     def delete(self, request):
         this_order_id = request.POST.get("order_id", "")
@@ -41,9 +38,6 @@ class OrderView(View):
         if this_delete_order:
             this_delete_order.is_delete = True
             this_delete_order.save()
+            self.order_list = Order.objects.all()
 
-        form = OrderPostForm()
-        product_list = Product.objects.all()
-        order_list = Order.objects.all()
-
-        return render(request, self.template_name, {"form": form, 'product_list': product_list, 'order_list': order_list})
+        return self.get(request)
