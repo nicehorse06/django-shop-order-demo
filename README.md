28f874e740a22fbdcf92183e0b3e6901e18f6898
Jimmy Ma <a468963@gmail.com>
2019-10-05 00:43:00 +0800

    新增bootstrap樣式

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 ## todo
 * UI 美化
     * 一些必要的提示文字，比如欄位目前沒有資料
+    * 把input的說明塞到input裡面
 * 刪除訂單(身份和庫存檢查限用decorator實作)
     * 備註:加入訂單時,若小於可購買量,前端需提示貨源不足 / 刪除訂單,庫存從0變回有值則提示商品到貨 
 * 請設計一排程,根據訂單記錄算出各個館別的1.總銷售金額 2.總銷售數量 3.總訂單數量
@@ -16,3 +17,10 @@
 * 使用docker佳
 * 部署至雲端服務佳
 * 單元測試
+
+### todo中的todo
+* django 匯出 csv
+* redis、celery建制
+* docker django hello world
+* 單元測試hello world
+* 部屬GCP
--- shop_web/order_page/forms.py
+++ shop_web/order_page/forms.py
@@ -7,6 +7,6 @@ class OrderPostForm(forms.Form):
     product_id = forms.ChoiceField(choices=Product.objects.values_list('product_id', 'product_id'),
                                    initial='vendor_code',
                                    label=u'產品編號')
-    quantity = forms.IntegerField()
+    quantity = forms.IntegerField(label='數量')
     customer_id = forms.IntegerField()
     is_vip = forms.BooleanField(required=False)
