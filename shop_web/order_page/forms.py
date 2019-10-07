9068e6581547cea54596235565630d8a726378d8
jimmyma <jimmyma@croxlink.com>
2019-10-07 10:55:24 +0800

    更改樣式並新增fontcdnjs on GitHub

--- shop_web/order_page/forms.py
+++ shop_web/order_page/forms.py
@@ -2,15 +2,21 @@ from django import forms
 
 
 class OrderPostForm(forms.Form):
-    product_id = forms.ChoiceField(initial='Select Product',
-                                   label=u'產品編號')
-    quantity = forms.IntegerField(label='數量')
-    customer_id = forms.IntegerField()
-    is_vip = forms.BooleanField(required=False)
+    product_id = forms.ChoiceField(label=u'產品編號')
+    quantity = forms.IntegerField(
+        label='數量', widget=forms.NumberInput(attrs={'placeholder': '數量'}))
+    customer_id = forms.IntegerField(
+        label='顧客 ID',
+        widget=forms.NumberInput(attrs={'placeholder': 'Customer ID'}))
+    is_vip = forms.BooleanField(required=False, label='是否為VIP身份')
+
     def __init__(self, *args, **kwargs):
         super(OrderPostForm, self).__init__(*args, **kwargs)
 
         this_field = self.fields.get('product_id')
         if this_field:
             from .models import Product
-            this_field.choices = Product.objects.values_list('product_id', 'product_id')
+            choices_value = [('', 'Select Product')]
+            choices_value.extend(Product.objects.values_list(
+                'product_id', 'product_id'))
+            this_field.choices = choices_value
